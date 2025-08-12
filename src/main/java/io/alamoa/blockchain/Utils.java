3f07f1f5dbff80586f32feb0e8b1ac9de1a07f3f
YASUYUKI UEDA <yasuyukiueda@YASUYUKInoMacBook-Air.local>
2025-08-12 10:14:41 +0200

    add util to convert string to key

--- src/main/java/io/alamoa/blockchain/Utils.java
+++ src/main/java/io/alamoa/blockchain/Utils.java
@@ -1,5 +1,10 @@
 package io.alamoa.blockchain;
 
+import java.security.*;
+import java.security.spec.InvalidKeySpecException;
+import java.security.spec.PKCS8EncodedKeySpec;
+import java.security.spec.X509EncodedKeySpec;
+import java.util.HexFormat;
 import java.util.LinkedHashMap;
 import java.util.Map;
 import java.util.stream.Collectors;
@@ -13,6 +18,20 @@ public class Utils {
                         Map.Entry::getKey,
                         Map.Entry::getValue,
                         (oldValue, newValue) -> oldValue, LinkedHashMap::new));
+    }
+
+    public static PublicKey convertStringToPublicKey(String keyHex) throws NoSuchAlgorithmException, InvalidKeySpecException {
+        byte[] keyBytes = HexFormat.of().parseHex(keyHex);
+        KeyFactory keyFactory = KeyFactory.getInstance("EC");
+        X509EncodedKeySpec keySpec = new X509EncodedKeySpec(keyBytes);
+        return keyFactory.generatePublic(keySpec);
+    }
+
+    public static PrivateKey convertStringToPrivateKey(String keyHex) throws NoSuchAlgorithmException, InvalidKeySpecException {
+        byte[] keyBytes = HexFormat.of().parseHex(keyHex);
+        KeyFactory keyFactory = KeyFactory.getInstance("EC");
+        PKCS8EncodedKeySpec keySpec = new PKCS8EncodedKeySpec(keyBytes);
+        return keyFactory.generatePrivate(keySpec);
     }
 }
 
