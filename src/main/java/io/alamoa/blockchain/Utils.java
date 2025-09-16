1821bdfc635e654f045a74390ce1432925454151
YASUYUKI UEDA <yasuyukiueda@Mac.lan>
2025-09-17 06:37:27 +0900

    format

--- src/main/java/io/alamoa/blockchain/Utils.java
+++ src/main/java/io/alamoa/blockchain/Utils.java
@@ -5,14 +5,14 @@ import java.util.Map;
 import java.util.stream.Collectors;
 
 public class Utils {
-    public static Map<String, Object> sortedMapByKey(Map<String, Object> unsortedMap) {
-        return unsortedMap.entrySet()
-                .stream()
-                .sorted(Map.Entry.comparingByKey())
-                .collect(Collectors.toMap(
-                        Map.Entry::getKey,
-                        Map.Entry::getValue,
-                        (oldValue, newValue) -> oldValue, LinkedHashMap::new));
-    }
+  public static Map<String, Object> sortedMapByKey(Map<String, Object> unsortedMap) {
+    return unsortedMap.entrySet().stream()
+        .sorted(Map.Entry.comparingByKey())
+        .collect(
+            Collectors.toMap(
+                Map.Entry::getKey,
+                Map.Entry::getValue,
+                (oldValue, newValue) -> oldValue,
+                LinkedHashMap::new));
+  }
 }
-
--- src/main/java/io/alamoa/blockchain/controller/TestController.java
+++ src/main/java/io/alamoa/blockchain/controller/TestController.java
@@ -15,44 +15,43 @@ import java.util.Map;
 @Controller
 public class TestController {
 
-    @Autowired
-    private Blockchain blockchain;
+  @Autowired private Blockchain blockchain;
 
-    @GetMapping("/test")
-    public String test(Model model) {
-        Gson gson = new GsonBuilder().setPrettyPrinting().disableHtmlEscaping().create();
+  @GetMapping("/test")
+  public String test(Model model) {
+    Gson gson = new GsonBuilder().setPrettyPrinting().disableHtmlEscaping().create();
 
-        //初期ブロック作成
-        String initBlock = gson.toJson(blockchain.getChain());
-        model.addAttribute("first", initBlock);
+    // 初期ブロック作成
+    String initBlock = gson.toJson(blockchain.getChain());
+    model.addAttribute("first", initBlock);
 
-        //transactionの追加
-        blockchain.addTransaction("A", "B", 10);
-        blockchain.addTransaction("C", "D", 20);
-        String transactionPoolBefore = gson.toJson(blockchain.getTransactionPool());
-        model.addAttribute("transactionPoolBefore", transactionPoolBefore);
+    // transactionの追加
+    blockchain.addTransaction("A", "B", 10);
+    blockchain.addTransaction("C", "D", 20);
+    String transactionPoolBefore = gson.toJson(blockchain.getTransactionPool());
+    model.addAttribute("transactionPoolBefore", transactionPoolBefore);
 
-        //blockの追加
-        blockchain.mine();
+    // blockの追加
+    blockchain.mine();
 
-        //block追加後のデータ状態の取得
-        String addBlock = gson.toJson(blockchain.getChain());
-        model.addAttribute("addedBlock", addBlock);
-        String transactionPoolAfter = gson.toJson(blockchain.getTransactionPool());
-        model.addAttribute("transactionPoolAfter", transactionPoolAfter);
+    // block追加後のデータ状態の取得
+    String addBlock = gson.toJson(blockchain.getChain());
+    model.addAttribute("addedBlock", addBlock);
+    String transactionPoolAfter = gson.toJson(blockchain.getTransactionPool());
+    model.addAttribute("transactionPoolAfter", transactionPoolAfter);
 
-        //nonceの検証(同じnonceを使ってハッシュ値を作成)
-        int nonce = (int) blockchain.getChain().get(1).get("nonce");
-        Map<String,Object> firstBlock = blockchain.getChain().get(0);
-        Map<String,Object> secondBlock = blockchain.getChain().get(1);
-        Map<String, Object> guessBlock = new LinkedHashMap<>();
-        guessBlock.put("transactions", secondBlock.get("transactions"));
-        guessBlock.put("previous_hash", blockchain.changeToHash(firstBlock));
-        guessBlock.put("nonce", nonce);
-        guessBlock = Utils.sortedMapByKey(guessBlock);
-        String guessHash = blockchain.changeToHash(guessBlock);
-        model.addAttribute("nonce",nonce);
-        model.addAttribute("reproducedHash",guessHash);
-        return "outputtest";
-    }
+    // nonceの検証(同じnonceを使ってハッシュ値を作成)
+    int nonce = (int) blockchain.getChain().get(1).get("nonce");
+    Map<String, Object> firstBlock = blockchain.getChain().get(0);
+    Map<String, Object> secondBlock = blockchain.getChain().get(1);
+    Map<String, Object> guessBlock = new LinkedHashMap<>();
+    guessBlock.put("transactions", secondBlock.get("transactions"));
+    guessBlock.put("previous_hash", blockchain.changeToHash(firstBlock));
+    guessBlock.put("nonce", nonce);
+    guessBlock = Utils.sortedMapByKey(guessBlock);
+    String guessHash = blockchain.changeToHash(guessBlock);
+    model.addAttribute("nonce", nonce);
+    model.addAttribute("reproducedHash", guessHash);
+    return "outputtest";
+  }
 }
