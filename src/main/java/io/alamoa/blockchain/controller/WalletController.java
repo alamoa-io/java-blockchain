e9b042d85ac2c62ba63ecf6ddaa892901b48b3aa
YASUYUKI UEDA <yasuyukiueda@Mac.lan>
2025-09-17 06:31:20 +0900

    format

--- src/main/java/io/alamoa/blockchain/controller/WalletController.java
+++ src/main/java/io/alamoa/blockchain/controller/WalletController.java
@@ -17,47 +17,45 @@ import org.springframework.web.client.RestTemplate;
 
 @Controller
 public class WalletController {
-    private final Gson gson = new GsonBuilder().setPrettyPrinting().disableHtmlEscaping().create();
-    @Autowired
-    private RestTemplate restTemplate;
+  private final Gson gson = new GsonBuilder().setPrettyPrinting().disableHtmlEscaping().create();
+  @Autowired private RestTemplate restTemplate;
 
-    @GetMapping("/wallet")
-    public String makeWallet(Model model) {
-        try {
-            Wallet wallet = new Wallet();
-            model.addAttribute("publicKey", wallet.getPublicKey());
-            model.addAttribute("privateKey", wallet.getPrivateKey());
-            model.addAttribute("blockChainAddress", wallet.getBlockchainAddress());
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        return "wallet";
+  @GetMapping("/wallet")
+  public String makeWallet(Model model) {
+    try {
+      Wallet wallet = new Wallet();
+      model.addAttribute("publicKey", wallet.getPublicKey());
+      model.addAttribute("privateKey", wallet.getPrivateKey());
+      model.addAttribute("blockChainAddress", wallet.getBlockchainAddress());
+    } catch (Exception e) {
+      e.printStackTrace();
     }
+    return "wallet";
+  }
 
-    @PostMapping("/addTransaction")
-    @ResponseBody
-    public ResponseEntity<String> addTransaction(@RequestBody TransactionRequest transactionRequest) {
-        try {
-            Wallet wallet = new Wallet();
-            Transaction transaction = new Transaction(transactionRequest.getSenderBlockchainAddress(),
-                    transactionRequest.getSenderBlockchainAddress(),
-                    transactionRequest.getAmount());
-            String transactionJson = gson.toJson(transaction.toMap());
-            String signature = wallet.generateSignature(transactionJson, transactionRequest.getSenderPrivateKey());
-            transactionRequest.setSignature(signature);
-            transactionRequest.setTransactionData(transaction.toMap());
+  @PostMapping("/addTransaction")
+  @ResponseBody
+  public ResponseEntity<String> addTransaction(@RequestBody TransactionRequest transactionRequest) {
+    try {
+      Wallet wallet = new Wallet();
+      Transaction transaction =
+          new Transaction(
+              transactionRequest.getSenderBlockchainAddress(),
+              transactionRequest.getSenderBlockchainAddress(),
+              transactionRequest.getAmount());
+      String transactionJson = gson.toJson(transaction.toMap());
+      String signature =
+          wallet.generateSignature(transactionJson, transactionRequest.getSenderPrivateKey());
+      transactionRequest.setSignature(signature);
+      transactionRequest.setTransactionData(transaction.toMap());
 
+      restTemplate.postForObject(
+          "http://localhost:8080//transactions", transactionRequest, String.class);
+      return ResponseEntity.ok("Transaction added successfully!");
 
-            restTemplate.postForObject(
-                    "http://localhost:8080//transactions",
-                    transactionRequest,
-                    String.class
-            );
-            return ResponseEntity.ok("Transaction added successfully!");
-
-        } catch (Exception e) {
-            System.err.println("Error processing transaction: " + e.getMessage());
-            return ResponseEntity.status(500).body("Error processing transaction.");
-        }
+    } catch (Exception e) {
+      System.err.println("Error processing transaction: " + e.getMessage());
+      return ResponseEntity.status(500).body("Error processing transaction.");
     }
+  }
 }
--- src/main/java/io/alamoa/blockchain/model/Transaction.java
+++ src/main/java/io/alamoa/blockchain/model/Transaction.java
@@ -7,25 +7,26 @@ import java.util.Map;
 
 public class Transaction {
 
-    private final String RECIPIENT_BLOCKCHAIN_ADDRESS = "recipient_blockchain_address";
-    private final String SENDER_BLOCKCHAIN_ADDRESS = "sender_blockchain_address";
-    private final String VALUE = "value";
+  private final String RECIPIENT_BLOCKCHAIN_ADDRESS = "recipient_blockchain_address";
+  private final String SENDER_BLOCKCHAIN_ADDRESS = "sender_blockchain_address";
+  private final String VALUE = "value";
 
-    private String senderBlockchainAddress;
-    private String recipientBlockchainAddress;
-    private double value;
+  private String senderBlockchainAddress;
+  private String recipientBlockchainAddress;
+  private double value;
 
-    public Transaction(String senderBlockchainAddress, String recipientBlockchainAddress, double value){
-        this.senderBlockchainAddress = senderBlockchainAddress;
-        this.recipientBlockchainAddress = recipientBlockchainAddress;
-        this.value = value;
-    }
+  public Transaction(
+      String senderBlockchainAddress, String recipientBlockchainAddress, double value) {
+    this.senderBlockchainAddress = senderBlockchainAddress;
+    this.recipientBlockchainAddress = recipientBlockchainAddress;
+    this.value = value;
+  }
 
-    public Map<String, Object> toMap(){
-        Map<String, Object> transactionData = new LinkedHashMap<>();
-        transactionData.put(RECIPIENT_BLOCKCHAIN_ADDRESS, this.recipientBlockchainAddress);
-        transactionData.put(SENDER_BLOCKCHAIN_ADDRESS, this.senderBlockchainAddress);
-        transactionData.put(VALUE, String.valueOf(value));
-        return Utils.sortedMapByKey(transactionData);
-    }
+  public Map<String, Object> toMap() {
+    Map<String, Object> transactionData = new LinkedHashMap<>();
+    transactionData.put(RECIPIENT_BLOCKCHAIN_ADDRESS, this.recipientBlockchainAddress);
+    transactionData.put(SENDER_BLOCKCHAIN_ADDRESS, this.senderBlockchainAddress);
+    transactionData.put(VALUE, String.valueOf(value));
+    return Utils.sortedMapByKey(transactionData);
+  }
 }
