4daa8f26b245a1ef8137dd088dfcdd4f030306c6
YASUYUKI UEDA <yasuyukiueda@Mac.lan>
2025-09-17 06:28:54 +0900

    format

--- src/main/java/io/alamoa/blockchain/entity/TransactionRequest.java
+++ src/main/java/io/alamoa/blockchain/entity/TransactionRequest.java
@@ -3,67 +3,67 @@ package io.alamoa.blockchain.entity;
 import java.util.Map;
 
 public class TransactionRequest {
-    private String senderPrivateKey;
-    private String senderBlockchainAddress;
-    private String recipientBlockchainAddress;
-    private String senderPublicKey;
-    private double amount;
-    private String signature;
-    private Map<String, Object> transactionData;
+  private String senderPrivateKey;
+  private String senderBlockchainAddress;
+  private String recipientBlockchainAddress;
+  private String senderPublicKey;
+  private double amount;
+  private String signature;
+  private Map<String, Object> transactionData;
 
-    public String getSenderPrivateKey() {
-        return senderPrivateKey;
-    }
+  public String getSenderPrivateKey() {
+    return senderPrivateKey;
+  }
 
-    public void setSenderPrivateKey(String senderPrivateKey) {
-        this.senderPrivateKey = senderPrivateKey;
-    }
+  public void setSenderPrivateKey(String senderPrivateKey) {
+    this.senderPrivateKey = senderPrivateKey;
+  }
 
-    public String getSenderBlockchainAddress() {
-        return senderBlockchainAddress;
-    }
+  public String getSenderBlockchainAddress() {
+    return senderBlockchainAddress;
+  }
 
-    public void setSenderBlockchainAddress(String senderBlockchainAddress) {
-        this.senderBlockchainAddress = senderBlockchainAddress;
-    }
+  public void setSenderBlockchainAddress(String senderBlockchainAddress) {
+    this.senderBlockchainAddress = senderBlockchainAddress;
+  }
 
-    public String getRecipientBlockchainAddress() {
-        return recipientBlockchainAddress;
-    }
+  public String getRecipientBlockchainAddress() {
+    return recipientBlockchainAddress;
+  }
 
-    public void setRecipientBlockchainAddress(String recipientBlockchainAddress) {
-        this.recipientBlockchainAddress = recipientBlockchainAddress;
-    }
+  public void setRecipientBlockchainAddress(String recipientBlockchainAddress) {
+    this.recipientBlockchainAddress = recipientBlockchainAddress;
+  }
 
-    public String getSenderPublicKey() {
-        return senderPublicKey;
-    }
+  public String getSenderPublicKey() {
+    return senderPublicKey;
+  }
 
-    public void setSenderPublicKey(String senderPublicKey) {
-        this.senderPublicKey = senderPublicKey;
-    }
+  public void setSenderPublicKey(String senderPublicKey) {
+    this.senderPublicKey = senderPublicKey;
+  }
 
-    public double getAmount() {
-        return amount;
-    }
+  public double getAmount() {
+    return amount;
+  }
 
-    public void setAmount(double amount) {
-        this.amount = amount;
-    }
+  public void setAmount(double amount) {
+    this.amount = amount;
+  }
 
-    public String getSignature() {
-        return signature;
-    }
+  public String getSignature() {
+    return signature;
+  }
 
-    public void setSignature(String signature) {
-        this.signature = signature;
-    }
+  public void setSignature(String signature) {
+    this.signature = signature;
+  }
 
-    public Map<String, Object> getTransactionData() {
-        return transactionData;
-    }
+  public Map<String, Object> getTransactionData() {
+    return transactionData;
+  }
 
-    public void setTransactionData(Map<String, Object> transactionData) {
-        this.transactionData = transactionData;
-    }
+  public void setTransactionData(Map<String, Object> transactionData) {
+    this.transactionData = transactionData;
+  }
 }
