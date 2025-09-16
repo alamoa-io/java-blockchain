dedf48f38f464950cd1cc11e82aed1a1f74e5c21
YASUYUKI UEDA <yasuyukiueda@Mac.lan>
2025-09-17 06:38:53 +0900

    format

--- src/main/java/io/alamoa/blockchain/controller/TestController.java
+++ src/main/java/io/alamoa/blockchain/controller/TestController.java
@@ -13,17 +13,16 @@ import java.util.Map;
 @Controller
 public class TestController {
 
-    @Autowired
-    private Blockchain blockchain;
+  @Autowired private Blockchain blockchain;
 
-    @GetMapping("/test")
-    public String test(Model model) {
-        Gson gson = new GsonBuilder().setPrettyPrinting().disableHtmlEscaping().create();
-        Map<String, Object> sampleBlock = blockchain.createBlock(0, "hash");
-        String sampleBlockJson = gson.toJson(sampleBlock);
-        model.addAttribute("json", sampleBlockJson);
-        String hashedSampleBlockJson = blockchain.changeToHash(sampleBlock);
-        model.addAttribute("hash", hashedSampleBlockJson);
-        return "outputtest";
-    }
+  @GetMapping("/test")
+  public String test(Model model) {
+    Gson gson = new GsonBuilder().setPrettyPrinting().disableHtmlEscaping().create();
+    Map<String, Object> sampleBlock = blockchain.createBlock(0, "hash");
+    String sampleBlockJson = gson.toJson(sampleBlock);
+    model.addAttribute("json", sampleBlockJson);
+    String hashedSampleBlockJson = blockchain.changeToHash(sampleBlock);
+    model.addAttribute("hash", hashedSampleBlockJson);
+    return "outputtest";
+  }
 }
